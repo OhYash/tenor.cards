403b0090cbf4120d63b8c019fff27a17f0627a86
Yash Yadav <yashdimpu@gmail.com>
2021-01-31 01:28:13 +0530

    add logo and encoding

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,19 @@
+const absolutePath = 'https://ohyash.github.io/webcard/';
+
+// To encode, use this
+// btoa(encodeURIComponent(""))
+
 function processData() {
 	var urlParams = new URLSearchParams(location.search);
 	if (urlParams.has('data'))
 	{
-		document.getElementById('MessageText').innerHTML = urlParams.get('data');
+		let dataParam = atob(urlParams.get('data')); // Get and URI decode
+		let decryptedDataParam = decodeURIComponent(dataParam); // base64 decode
+
+		document.getElementById('MessageText').innerHTML = decryptedDataParam;
+	}
+	else if(window.location.href != absolutePath)
+	{
+		window.location.href = absolutePath;
 	}
 }
