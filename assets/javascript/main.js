ee88090a60766ab3cf2fbdb6abc43eb95659b541
Yash Yadav <yashdimpu@gmail.com>
2021-02-21 02:56:37 +0530

    added textarea and responsiveness updates

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,13 @@
-const absolutePath = 'https://ohyash.github.io/webcard/';
+const absolutePath = 'https://tenor.cards/';
 
 // To encode, use this
 // btoa(encodeURIComponent(""))
 
-function processData() {
+function processPathParams() {
 	var urlParams = new URLSearchParams(location.search);
-	if (urlParams.has('data'))
+	if (urlParams.has('p'))
 	{
-		let dataParam = atob(urlParams.get('data')); // Get and URI decode
+		let dataParam = atob(urlParams.get('p')); // Get and URI decode
 		let decryptedDataParam = decodeURIComponent(dataParam); // base64 decode
 
 		document.getElementById('MessageText').innerHTML = decryptedDataParam;
@@ -17,4 +17,12 @@ function processData() {
 		// Commenting temporarily as create page is not yet ready
 		// window.location.href = absolutePath;
 	}
+}
+
+function dispTextCount() {
+	let text = document.getElementById("textInput").value;
+	let textLength = text.length;
+	//if (length === 140)
+		// set color of 'inputCountStat' object to light red  	
+	document.getElementById("inputCountStat").innerHTML = textLength;
 }
